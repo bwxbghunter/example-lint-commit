762a85a3b6ee7b40ea785892e302d0fb0e162a5e
部文旭 <buwenxu@xdf.cn>
2023-04-12 15:59:00 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
+
+function test (a, b) {
+  console.log(a, b);
+}
+test(1, 3);
