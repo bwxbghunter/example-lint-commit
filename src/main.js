6ccd5f8890dcc5b018502275651a0ee2bc242153
部文旭 <buwenxu@xdf.cn>
2023-04-12 11:01:16 +0800

    test prettier

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
 
 function test() {
-console.log("测试 prettier");  // 将缩进去掉, 同时使用双引号"
+    console.log('测试 prettier'); // 将缩进去掉, 同时使用双引号"
 }
