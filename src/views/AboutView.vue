81ec226d47c956ef1b05d8d0bf190bfb6bfe62c7
部文旭 <buwenxu@xdf.cn>
2023-05-23 22:16:23 +0800

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
-    <div>test eslint prettier lint-staged</div>
+<div>test eslint prettier lint-staged</div>
     </div>
 </template>
 
